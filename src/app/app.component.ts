175768efdb25742b1e2aaef73c44977762845da5
Alexander <car3651@yandex.ru>
2020-11-12 20:37:53 +0500

    added rainbow table, hide button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-angular';
+
+  rowClass = ''
+
+  red = 'background-color: rgb(255,000,000);'
+  orange = 'background-color: rgb(255, 128, 0);'
+  yellow = 'background-color: rgb(255,255,000);'
+  green = 'background-color: rgb(000,255,000);'
+  teal = 'background-color: rgb(000,255,255);'
+  blue = 'background-color: rgb(000,000,255);'
+  purple = 'background-color: rgb(128,000,128);'
+
+  hideTable() {
+    this.rowClass = 'collapse'
+  }
 }
